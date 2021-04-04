6a4b5b63d6090eccb46515320c3e9b8cf9c4c31e
ryotaro kurita <ryo.kuri2525@gmail.com>
2021-04-04 23:23:01 +0900

    add_v_model

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,83 @@
 <template>
   <div>
-    <like-header header-text="hello">
-      <template v-slot:title>
-        <h2>こんにちは</h2>
-      </template>
-      <template v-slot:number>
-        <p>{{number}}</p>
-      </template>
-    </like-header>  
+    <likeHeader>
+        <h3>はじめまして</h3>
+    </likeHeader>  
     <LikeNumber v-bind:total-number='number' @my-click="incrementNumber"></LikeNumber>
-    <LikeNumber :totalNumber='number'> </LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <keep-alive>
+      <component :is="currentComponent"></component>
+    </keep-alive>
+    <div style="padding:10rem;">
+      <h2>イベントのフォーム</h2>
+      <label for="title">タイトル</label>
+      <input id="title" type="text" :value="eventData.title" @input="eventData.title = $event.target.value">
+      <pre>{{eventData.title}}</pre>
+      <label for="maxNumber">最大人数</label>
+      <input id="maxNumber" type="number" v-model.number="eventData.maxNumber">
+      <p>{{ typeof eventData.maxNumber}}</p>
+      <label for="host">主催者</label>
+      <input id="host" type="text" v-model.trim="eventData.host">
+      <pre>{{eventData.host}}</pre>
+      <label for="detail">イベントの内容</label>
+      <textarea name="detail" id="30" cols="30" v-model="eventData.detail"></textarea>
+      <p style="white-space: pre;">{{eventData.detail}}</p>
+      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+      <label for="isPrivate">非公開</label>
+      <p>{{eventData.isPrivate}}</p>
+      
+      <p>参加条件</p>
+      <input type="checkbox" id="10" value="10代" v-model="eventData.target">
+      <label for="10">10代</label>
+      <input type="checkbox" id="20" value="20代" v-model="eventData.target">
+      <label for="20">20代</label>
+      <input type="checkbox" id="30" value="30代" v-model="eventData.target">
+      <label for="30">30代</label>
+      <p>{{eventData.target}}</p>
+
+      <p>参加費</p>
+      <input type="radio" id="free" value="無料" v-model="eventData.price">
+      <label for="free">無料</label>
+      <input type="radio" id="paid" value="有料" v-model="eventData.price">
+      <label for="paid">有料</label>
+
+      <p>開催場所</p>
+      <select v-model="eventData.location" multiple>
+        <option v-for="location in locations" :key="location">{{location}}</option>
+      </select>
+      <p>{{eventData.location}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import LikeHeader from "./componets/LikeHeader.vue";
+import LikeHeader from "./components/LikeHeader.vue";
+import About from "./components/About.vue";
+import Home from "./components/Home.vue";
 
 export default {
   data() {
     return {
-      number: 14
+      number: 14,
+      currentComponent: 'Home',
+      locations: ["東京", "大阪", "名古屋"],
+      eventData: {
+        title: "タイトル",
+        maxNumber: 0,
+        host: "",
+        detail: "",
+        isPrivate: false,
+        target: [],
+        price: "無料",
+        location: []
+      }
     };
   },
   components: {
-    LikeHeader
+    LikeHeader,
+    About,
+    Home
   },
   methods: {
     incrementNumber(value) {
@@ -32,16 +86,4 @@ export default {
   }
 }
 </script>
-
-<style>
- div {
-   border: 1px solid blue;
- }
-
- h1 {
-   color: red;
- }
-</style>
-
-
 
