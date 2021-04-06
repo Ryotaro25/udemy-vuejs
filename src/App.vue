75243ad969d73d855ef78289351fac213e4d74bc
ryotaro kurita <ryo.kuri2525@gmail.com>
2021-04-06 23:56:37 +0900

    animation

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
     <transition name="fade">
       <p v-if="show">hello</p>
     </transition>
+    <transition name="slide" type="animation">
+    <p v-if="show">bye</p>
+    
+    </transition>
   </div>
 </template>
 
@@ -35,6 +39,30 @@ export default {
   }
   .fade-leave-to {
     opacity: 0;
+  }
+
+  .slide-enter,
+  .slide-leave-to {
+    opacity: 0;
+  }
+  .slide-enter-active {
+    animation: slide-in 0.5s;
+    opacity: 0.5s;
+  }
+  
+  .slide-leave-active {
+    animation: slide-in 0.5s reverse;
+    opacity: 0.5s;
+  }
+
+
+  @keyframes slide-in {
+    from {
+      transform: translateX(100px);
+    }
+    to {
+      transform: translateX(0);
+    }
   }
   
   .main {
